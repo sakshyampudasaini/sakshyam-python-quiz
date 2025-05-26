35a10e4d993201ef076dc3325098dca86a377ba6
sakshyampudasaini <123820304+sakshyampudasaini@users.noreply.github.com>
2025-05-26 22:31:31 +0545

    Add Python console quiz with multiple-choice questions and attempt limits
    A beginner-level Python quiz application in the console that asks multiple-choice questions with a limited number of attempts per question.

---
+++ quiz_game.py
@@ -0,0 +1,124 @@
+attempt = 0
+max_attempts = 3
+
+while attempt < max_attempts:
+    print("1. What is the capital of France?")
+    print("A) London")
+    print("B) Berlin")
+    print("C) Paris")
+    print("D) Madrid")
+
+    user = input("Your answer: ")
+    if user.lower() == "c":
+        print("Correct Answer")
+        break
+    else:
+        print("Try another one")
+        attempt += 1
+
+if attempt == max_attempts:
+    print("No more attempts left for this question.")
+
+
+attempt = 0
+max_attempts = 3
+
+while attempt<max_attempts:
+   
+    print("2. Which planet is known as the Red Planet?")
+    print("A) Earth")
+    print("B) Mars")
+    print("C) Jupiter")
+    print("D) Venus")
+    user = input("Your answer: ")
+
+    if user.lower() =="b":
+        print("Correct Answer")
+        break
+    else:
+        print("Try again")
+        attempt += 1
+
+if attempt == max_attempts:
+    print("No more attempts left for this question.")
+
+
+attempt = 0
+max_attempts = 3
+while attempt<max_attempts:
+  
+
+    print("3. What is 5 + 7?")
+    print("A) 10")
+    print("B) 12")
+    print("C) 14")
+    print("D) 11")
+    user = input("Your answer: ")
+
+    if user.lower()=="b":
+        print("Congratulation")
+        break
+    else:
+        print("Try again ")
+        attempt += 1
+
+if attempt == max_attempts:
+    print("No more attempts left for this question.")
+
+
+attempt = 0
+max_attempts = 3
+while attempt<max_attempts:
+    print("4. Which animal is known as the King of the Jungle?")
+    print("A) Tiger")
+    print("B) Lion")
+    print("C) Elephant")
+    print("D) Cheetah")
+    user = input("Your answer: ")
+    if user.lower()=="b":
+
+        print("Good job")
+        break
+    else:
+        print("Try again")
+        attempt += 1
+
+if attempt == max_attempts:
+    print("No more attempts left for this question.")
+
+
+attempt = 0
+max_attempts = 3
+while attempt<max_attempts:
+
+    print("5. What color do you get when you mix red and white?")
+    print("A) Pink")
+    print("B) Purple")
+    print("C) Orange")
+    print("D) Brown")
+    user = input("Your answer: ")
+
+    if user.lower()=="a":
+    
+
+        print("Correct Answer")
+        break
+    else:
+        print("Try again")
+        attempt += 1
+
+if attempt == max_attempts:
+    print("No more attempts left for this question.")
+  
+
+
+
+
+
+
+
+    
+
+
+
+
